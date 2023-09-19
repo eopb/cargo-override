60267434f608398f13b5abb13ba012b47d4ad003
Ethan Brierley <ethanboxx@gmail.com>
2023-09-19 19:40:04 +0100

    Add source as arg

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,9 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
-
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+    source: String,
 }
 
 fn main() {
     let args = Args::parse();
-
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
-    }
 }
