9243b8b48f2fc076bc0619c422990a5d44f15893
Ethan Brierley <ethanboxx@gmail.com>
2024-04-03 21:26:52 +0100

    test: patch path exists

---
+++ tests/tests.rs
@@ -0,0 +1,19 @@
+use std::fs;
+
+use cargo_override::{run, Args};
+
+use fake::{Fake, Faker};
+use tempfile::TempDir;
+
+#[test]
+fn patch_path_exists() {
+    let working_dir = TempDir::new().unwrap();
+    let working_dir = working_dir.path();
+
+    let patch_folder: String = Faker.fake();
+    let patch_folder_path = working_dir.join(&patch_folder);
+
+    fs::create_dir(patch_folder_path).expect("failed to create patch folder");
+
+    run(working_dir, Args { path: patch_folder })
+}
