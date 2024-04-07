0d6b39493ebbcab7489e12cf6935d36510175d9e
Ethan Brierley <ethanboxx@gmail.com>
2024-04-07 18:10:25 +0100

    feat: fail when patch does not have a `Cargo.toml` manifest

--- src/lib.rs
+++ src/lib.rs
@@ -17,5 +17,14 @@ pub fn run(working_dir: &Path, args: Args) -> anyhow::Result<()> {
         bail!("relative path \"{}\" is not a directory", args.path);
     }
 
+    let patch_manifest = patch_workspace.join("Cargo.toml");
+
+    if patch_manifest.is_file().not() {
+        bail!(
+            "relative path \"{}\" does not contain a `Cargo.toml` file",
+            args.path
+        )
+    }
+
     Ok(())
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, fs::File};
 
 use cargo_override::{run, Args};
 
@@ -17,7 +17,11 @@ fn patch_path_exists() {
     let patch_folder: String = Faker.fake();
     let patch_folder_path = working_dir.join(&patch_folder);
 
-    fs::create_dir(patch_folder_path).expect("failed to create patch folder");
+    fs::create_dir(&patch_folder_path).expect("failed to create patch folder");
+
+    let patch_manifest = patch_folder_path.join(&"Cargo.toml");
+
+    File::create_new(&patch_manifest).expect("failed to create patch manifest file");
 
     let result = run(working_dir, Args { path: patch_folder });
     expect_that!(result, ok(eq(())))
@@ -41,6 +45,32 @@ fn patch_path_doesnt_exist() {
         result,
         err(displays_as(eq(format!(
             "relative path \"{}\" is not a directory",
+            patch_folder
+        ))))
+    )
+}
+
+#[googletest::test]
+fn patch_manifest_doesnt_exist() {
+    let working_dir = TempDir::new().unwrap();
+    let working_dir = working_dir.path();
+
+    let patch_folder: String = Faker.fake();
+    let patch_folder_path = working_dir.join(&patch_folder);
+
+    fs::create_dir(patch_folder_path).expect("failed to create patch folder");
+
+    let result = run(
+        working_dir,
+        Args {
+            path: patch_folder.clone(),
+        },
+    );
+
+    expect_that!(
+        result,
+        err(displays_as(eq(format!(
+            "relative path \"{}\" does not contain a `Cargo.toml` file",
             patch_folder
         ))))
     )
