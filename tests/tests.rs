cc7a093d5d1525e8cfedd7bcb802a395df4cfc4e
AjithPanneerselvam <ajithpanneerselvam20@gmail.com>
2024-05-15 23:28:08 +0100

    chore: minor

--- tests/tests.rs
+++ tests/tests.rs
@@ -52,6 +52,7 @@ fn manifest_header(crate_name: &str) -> String {
 name = \"{crate_name}\"
 version = \"0.1.0\"
 edition = \"2021\"
+
 # See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
 "
     )
