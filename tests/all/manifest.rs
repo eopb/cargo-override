69e811b37859d1ed79db0f711b63407be58b0a71
Alexander Hübener <alex.teamplayer@gmail.com>
2024-09-14 18:24:38 +0200

    add add_patch to Manifest

--- tests/all/manifest.rs
+++ tests/all/manifest.rs
@@ -1,10 +1,11 @@
 //! Builders for generating manifest files for tests
 
-use std::fmt::Write as _;
+use std::{collections::HashMap, fmt::Write as _};
 
 pub struct Manifest {
     header: Header,
     dependencies: Dependencies,
+    patches: Patches,
     target: Option<Target>,
 }
 
@@ -13,6 +14,7 @@ impl Manifest {
         Self {
             header,
             dependencies: Dependencies::new(),
+            patches: Patches::new(),
             target: None,
         }
     }
@@ -27,10 +29,16 @@ impl Manifest {
         self
     }
 
+    pub fn add_patch(mut self, registry: &str, patch: Patch) -> Self {
+        self.patches.add(registry, patch);
+        self
+    }
+
     pub fn render(self) -> String {
         let Self {
             header,
             dependencies,
+            patches,
             target,
         } = self;
 
@@ -38,6 +46,7 @@ impl Manifest {
 
         writeln!(w, "{}", header.render()).unwrap();
         write!(w, "{}", dependencies.render()).unwrap();
+        write!(w, "{}", patches.render()).unwrap();
         if let Some(target) = target {
             write!(w, "{}", target.render()).unwrap();
         }
@@ -239,6 +248,145 @@ impl Dependency {
     }
 }
 
+struct Patches {
+    patches: HashMap<String, Vec<Patch>>,
+}
+
+impl Patches {
+    pub fn new() -> Self {
+        Self {
+            patches: HashMap::new(),
+        }
+    }
+
+    fn add(&mut self, registry: &str, patch: Patch) {
+        self.patches
+            .entry(registry.to_owned())
+            .or_insert_with(Vec::new)
+            .push(patch);
+    }
+
+    fn render(self) -> String {
+        let Self { patches } = self;
+
+        let mut f = String::new();
+        for (registry, patches) in patches {
+            writeln!(f, "[patch.{registry}]", registry = registry).unwrap();
+            for patch in patches {
+                writeln!(f, "{}", patch.render()).unwrap();
+            }
+        }
+        f
+    }
+}
+
+pub enum PatchType {
+    Git(GitPatch),
+    Local(LocalPatch),
+}
+
+impl PatchType {
+    fn render(&self) -> String {
+        match self {
+            Self::Git(git) => git.render(),
+            Self::Local(local) => local.render(),
+        }
+    }
+}
+
+pub struct Patch {
+    name: String,
+    package: Option<String>,
+    r#type: PatchType,
+}
+
+impl Patch {
+    pub fn new(name: impl AsRef<str>, r#type: PatchType) -> Self {
+        Self {
+            name: name.as_ref().to_owned(),
+            package: None,
+            r#type,
+        }
+    }
+
+    /// https://doc.rust-lang.org/cargo/reference/overriding-dependencies.html#using-patch-with-multiple-versions
+    pub fn new_renamed(name: impl AsRef<str>, package: impl AsRef<str>, r#type: PatchType) -> Self {
+        Self {
+            name: name.as_ref().to_owned(),
+            package: Some(package.as_ref().to_owned()),
+            r#type,
+        }
+    }
+
+    fn render(self) -> String {
+        let Self {
+            name,
+            package,
+            r#type,
+        } = self;
+
+        let mut f = String::new();
+        write!(f, "{name} = {{", name = name).unwrap();
+        if let Some(package) = package {
+            write!(f, " package = \"{package}\",", package = package).unwrap();
+        }
+        write!(f, " {t} }}", t = r#type.render()).unwrap();
+        f
+    }
+}
+
+pub struct GitPatch {
+    url: String,
+    branch: Option<String>,
+    rev: Option<String>,
+}
+
+impl GitPatch {
+    pub fn new(
+        url: impl AsRef<str>,
+        branch: Option<impl AsRef<str>>,
+        rev: Option<impl AsRef<str>>,
+    ) -> Self {
+        Self {
+            url: url.as_ref().to_owned(),
+            branch: branch.map(|b| b.as_ref().to_owned()),
+            rev: rev.map(|r| r.as_ref().to_owned()),
+        }
+    }
+
+    fn render(&self) -> String {
+        let Self { url, branch, rev } = self;
+
+        let mut f = String::new();
+        write!(f, "git = \"{url}\"", url = url).unwrap();
+        if let Some(branch) = branch {
+            write!(f, ", branch = \"{branch}\"", branch = branch).unwrap();
+        }
+        if let Some(rev) = rev {
+            write!(f, ", rev = \"{rev}\"", rev = rev).unwrap();
+        }
+        f
+    }
+}
+
+pub struct LocalPatch {
+    path: String,
+}
+
+impl LocalPatch {
+    pub fn new(path: impl AsRef<str>) -> Self {
+        Self {
+            path: path.as_ref().to_owned(),
+        }
+    }
+
+    fn render(&self) -> String {
+        let Self { path } = self;
+
+        format!("path = \"{path}\"", path = path)
+    }
+}
+
 #[test]
 fn manifest_with_deps() {
     let header = Header::basic("package-name");
@@ -279,6 +427,61 @@ fn basic_manifest() {
 
     # See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
 
+    '''
+    "###);
+}
+
+#[test]
+fn manifest_with_patches() {
+    let header = Header::basic("package-name");
+
+    let manifest = Manifest::new(header)
+        .add_patch(
+            "crates-io",
+            Patch::new(
+                "test",
+                PatchType::Git(GitPatch::new(
+                    "https://github.com/test/test.git",
+                    Option::<&str>::None,
+                    Option::<&str>::None,
+                )),
+            ),
+        )
+        .add_patch(
+            "crates-io",
+            Patch::new_renamed(
+                "test2",
+                "test",
+                PatchType::Git(GitPatch::new(
+                    "https://github.com/test/test2.git",
+                    Some("main"),
+                    Some("324hb34"),
+                )),
+            ),
+        )
+        .add_patch(
+            "crates-io",
+            Patch::new_renamed(
+                "test3",
+                "test",
+                PatchType::Local(LocalPatch::new("/path/to/local/crate/test3")),
+            ),
+        )
+        .render();
+
+    insta::assert_toml_snapshot!(manifest, @r###"
+    '''
+    [package]
+    name = "package-name"
+    version = "0.1.0"
+    edition = "2021"
+
+    # See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
+
+    [patch.crates-io]
+    test = { git = "https://github.com/test/test.git" }
+    test2 = { package = "test", git = "https://github.com/test/test2.git", branch = "main", rev = "324hb34" }
+    test3 = { package = "test", path = "/path/to/local/crate/test3" }
     '''
     "###);
 }
